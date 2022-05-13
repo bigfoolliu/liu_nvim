9ae27b3b762170efe206859fc4b2484840ce3b63
刘瑞 <liurui07@inspur.com>
2022-05-13 13:28:03 +0800

    代码片段

---
+++ lua/basic/config.lua
@@ -0,0 +1,3 @@
+-- 指定代码片段存储路径，这个目录我们在第二章节的时候已经创建好了
+vim.g.vsnip_snippet_dir = "~/.config/nvim/snippet"
+
--- lua/conf/nvim-lsp-installer.lua
+++ lua/conf/nvim-lsp-installer.lua
@@ -1,5 +1,10 @@
 -- https://github.com/williamboman/nvim-lsp-installer
 local lsp_installer_servers = require("nvim-lsp-installer.servers")
+
+-- 使用 cmp_nvim_lsp 代替内置 omnifunc，获得更强的补全体验
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+
 -- WARN: 手动书写 LSP 配置文件
 -- 名称：https://github.com/williamboman/nvim-lsp-installer#available-lsps
 -- 配置：https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
@@ -65,6 +70,10 @@ for server_name, server_options in pairs(servers) do
                 server_options.flags = {
                     debounce_text_changes = 150
                 }
+
+                -- 代替内置 omnifunc
+                server_options.capabilities = capabilities
+
                 -- 启动服务
                 server:setup(server_options)
             end
