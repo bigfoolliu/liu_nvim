f0fda6c3a5708572a3224c1448e4a6dc626a48f4
刘瑞 <liurui07@inspur.com>
2022-05-13 17:50:32 +0800

    autosave 保存格式化

--- lua/conf/AutoSave.lua
+++ lua/conf/AutoSave.lua
@@ -6,7 +6,11 @@ require("autosave").setup(
         -- 触发自动保存的事件（退出插入模式或者普通模式下文本内容发生改变）
         events = {"InsertLeave", "TextChanged"},
         -- 自动保存时的提示信息
-        execution_message = "AutoSave done",
+        -- execution_message = "AutoSave done",
+        -- 同时提示保存的时间
+        execution_message = function ()
+            return "AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S")
+        end,
         conditions = {
             exists = true,
             -- 忽略自动保存的文件名字或文件类型
